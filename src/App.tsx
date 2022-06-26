361055e9aa1be92d3e42c6cfe0c323e62113bacd
Domianidze <sdomianidze123@gmail.com>
2022-06-26 20:30:06 +0400

    feat: set up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Routes, Route } from 'react-router-dom';
+
 import { Landing } from 'pages';
 
 const App: React.FC = () => {
   return (
-    <div className='App'>
-      <Landing />
-    </div>
+    <Routes>
+      <Route path='/:memberId' element={<Landing />} />
+      <Route index element={<Landing />} />
+    </Routes>
   );
 };
 
